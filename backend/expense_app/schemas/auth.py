c00d76ec813147366bd92378e62610c8818ec9fb
Rajkumar <rajkumar.s@beinex.com>
2026-05-23 17:55:08 +0530

    feat: authentication_jwt_feature

--- backend/expense_app/schemas/auth.py
+++ backend/expense_app/schemas/auth.py
@@ -5,3 +5,10 @@ from pydantic import EmailStr
 class LoginSchema(BaseModel):
     email: EmailStr
     password: str
+
+class ForgotPasswordSchema(BaseModel):
+    email : EmailStr
+
+class ResetPasswordSchema(BaseModel):
+    token : str
+    new_password :str
